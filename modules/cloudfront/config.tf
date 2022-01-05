21471392912b0b85316c913ecd2c8a0c3218359f
ArerepadeB <aredelulu@gmail.com>
2022-01-05 10:26:14 -0600

    cloudfront module

---
+++ modules/cloudfront/config.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">=0.12"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = "default"
+}
